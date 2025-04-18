e4ac21c7eca05da2500c8d8fa4b186b7b4e9aab6
RAJ VERMA <vraj96941@gmail.com>
2025-04-18 15:11:25 +0530

    product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -24,12 +24,12 @@ const Product = () => {
     const getProduct = async () => {
       setLoading(true);
       setLoading2(true);
-      const response = await fetch(`https://fakestoreapi.com/products/${id}`);
+      const response = await fetch(https://fakestoreapi.com/products/${id});
       const data = await response.json();
       setProduct(data);
       setLoading(false);
       const response2 = await fetch(
-        `https://fakestoreapi.com/products/category/${data.category}`
+        https://fakestoreapi.com/products/category/${data.category}
       );
       const data2 = await response2.json();
       setSimilarProducts(data2);
@@ -191,4 +191,4 @@ const Product = () => {
   );
 };
 
-export default Product;
+export default Product;          
